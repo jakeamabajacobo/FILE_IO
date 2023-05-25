4c9850877e043fee9a89017d8d518ea498e616fb
Jake Jacobo <jakejacobo2015@gmail.com>
2023-05-25 12:07:09 +0800

    Add project files.

---
+++ FILE_IO/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txt_box = New System.Windows.Forms.TextBox()
+        Me.btn_save = New System.Windows.Forms.Button()
+        Me.btn_open = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txt_box
+        '
+        Me.txt_box.Location = New System.Drawing.Point(12, 21)
+        Me.txt_box.Multiline = True
+        Me.txt_box.Name = "txt_box"
+        Me.txt_box.Size = New System.Drawing.Size(382, 314)
+        Me.txt_box.TabIndex = 0
+        '
+        'btn_save
+        '
+        Me.btn_save.Location = New System.Drawing.Point(220, 350)
+        Me.btn_save.Name = "btn_save"
+        Me.btn_save.Size = New System.Drawing.Size(125, 36)
+        Me.btn_save.TabIndex = 1
+        Me.btn_save.Text = "SAVE"
+        Me.btn_save.UseVisualStyleBackColor = True
+        '
+        'btn_open
+        '
+        Me.btn_open.Location = New System.Drawing.Point(62, 350)
+        Me.btn_open.Name = "btn_open"
+        Me.btn_open.Size = New System.Drawing.Size(125, 36)
+        Me.btn_open.TabIndex = 2
+        Me.btn_open.Text = "OPEN"
+        Me.btn_open.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(406, 450)
+        Me.Controls.Add(Me.btn_open)
+        Me.Controls.Add(Me.btn_save)
+        Me.Controls.Add(Me.txt_box)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txt_box As TextBox
+    Friend WithEvents btn_save As Button
+    Friend WithEvents btn_open As Button
+End Class
---
+++ FILE_IO/Form1.vb
@@ -0,0 +1,103 @@
+﻿Imports System.IO
+Imports System.IO.File
+Imports System.Diagnostics
+Imports System.Security.Cryptography
+Imports System.Net.WebRequestMethods
+Imports System.Text
+
+Public Class Form1
+    Dim open_Dlg As New OpenFileDialog
+    Dim strm_rdr As StreamReader
+    Dim strm_wtr As StreamWriter
+    Dim dir As String
+
+
+
+
+    Private Sub btn_save_Click(sender As Object, e As EventArgs) Handles btn_save.Click
+        Try
+            If (IO.File.Exists(dir)) Then
+
+                Try
+                    Dim buffer As [Byte]() = Encoding.ASCII.GetBytes(txt_box.Text)
+                    Dim MS As MemoryStream = New MemoryStream(buffer)
+
+                    If (IO.File.Exists(dir)) Then
+                        Dim File As FileStream = New FileStream(dir, FileMode.Create, FileAccess.Write)
+                    Else
+
+                        If sv_dialog.ShowDialog = Windows.Forms.DialogResult.OK Then
+
+
+                        End If
+                    End If
+
+                
+
+                    MS.WriteTo(File)
+                    File.Close()
+                    MS.Close()
+
+
+                    MsgBox("File saved!")
+                    txt_box.Clear()
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+
+
+            Else
+                Dim sv_dialog As New SaveFileDialog
+
+                sv_dialog.Filter = "TXT Files (*.txt*)|*.txt"
+
+                If sv_dialog.ShowDialog = Windows.Forms.DialogResult.OK Then
+                    My.Computer.FileSystem.WriteAllText(sv_dialog.FileName, txt_box.Text, False)
+                End If
+
+
+
+                MsgBox("File saved!")
+                txt_box.Clear()
+
+
+            End If
+
+
+
+
+
+
+            ' strm_writer.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btn_open_Click(sender As Object, e As EventArgs) Handles btn_open.Click
+
+
+        open_Dlg.Filter = "Text [*.txt*]|*.txt|All Files[*.*]|*.*"
+        open_Dlg.Title = "Select Text File"
+
+        If open_Dlg.ShowDialog = Windows.Forms.DialogResult.OK Then
+            dir = Convert.ToString(open_Dlg.FileName)
+            Try
+
+                Using fileStream As FileStream = New FileStream(dir, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, FileMode.Append)
+                    Dim sr As StreamReader = New StreamReader(fileStream)
+                    Dim Str As String = sr.ReadToEnd()
+                    txt_box.Text = Str
+                    sr.Dispose()
+                    sr.Close()
+                    fileStream.Dispose()
+                    fileStream.Close()
+                End Using
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End If
+    End Sub
+End Class
