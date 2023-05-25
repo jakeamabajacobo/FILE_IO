3f0c902602ecd8796b1821317386be07fe124990
J Jacobs <jakejacobo2015@gmail.com>
2023-05-25 14:20:40 +0800

    added image upload, that has function upload to local folder in C
    added

--- FILE_IO/Form1.Designer.vb
+++ FILE_IO/Form1.Designer.vb
@@ -22,14 +22,21 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.txt_box = New System.Windows.Forms.TextBox()
         Me.btn_save = New System.Windows.Forms.Button()
         Me.btn_open = New System.Windows.Forms.Button()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.pic_box = New System.Windows.Forms.PictureBox()
+        Me.btn_upload = New System.Windows.Forms.Button()
+        Me.btn_save_image = New System.Windows.Forms.Button()
+        CType(Me.pic_box, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'txt_box
         '
-        Me.txt_box.Location = New System.Drawing.Point(12, 21)
+        Me.txt_box.Location = New System.Drawing.Point(12, 76)
         Me.txt_box.Multiline = True
         Me.txt_box.Name = "txt_box"
         Me.txt_box.Size = New System.Drawing.Size(382, 314)
@@ -37,7 +44,7 @@ Partial Class Form1
         '
         'btn_save
         '
-        Me.btn_save.Location = New System.Drawing.Point(220, 350)
+        Me.btn_save.Location = New System.Drawing.Point(184, 415)
         Me.btn_save.Name = "btn_save"
         Me.btn_save.Size = New System.Drawing.Size(125, 36)
         Me.btn_save.TabIndex = 1
@@ -46,23 +53,62 @@ Partial Class Form1
         '
         'btn_open
         '
-        Me.btn_open.Location = New System.Drawing.Point(62, 350)
+        Me.btn_open.Location = New System.Drawing.Point(53, 415)
         Me.btn_open.Name = "btn_open"
         Me.btn_open.Size = New System.Drawing.Size(125, 36)
         Me.btn_open.TabIndex = 2
         Me.btn_open.Text = "OPEN"
         Me.btn_open.UseVisualStyleBackColor = True
         '
+        'ImageList1
+        '
+        Me.ImageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.ImageList1.ImageSize = New System.Drawing.Size(16, 16)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'pic_box
+        '
+        Me.pic_box.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pic_box.InitialImage = CType(resources.GetObject("pic_box.InitialImage"), System.Drawing.Image)
+        Me.pic_box.Location = New System.Drawing.Point(427, 76)
+        Me.pic_box.Name = "pic_box"
+        Me.pic_box.Size = New System.Drawing.Size(382, 314)
+        Me.pic_box.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic_box.TabIndex = 3
+        Me.pic_box.TabStop = False
+        '
+        'btn_upload
+        '
+        Me.btn_upload.Location = New System.Drawing.Point(488, 415)
+        Me.btn_upload.Name = "btn_upload"
+        Me.btn_upload.Size = New System.Drawing.Size(125, 36)
+        Me.btn_upload.TabIndex = 5
+        Me.btn_upload.Text = "UPLOAD"
+        Me.btn_upload.UseVisualStyleBackColor = True
+        '
+        'btn_save_image
+        '
+        Me.btn_save_image.Location = New System.Drawing.Point(619, 415)
+        Me.btn_save_image.Name = "btn_save_image"
+        Me.btn_save_image.Size = New System.Drawing.Size(125, 36)
+        Me.btn_save_image.TabIndex = 4
+        Me.btn_save_image.Text = "SAVE"
+        Me.btn_save_image.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(406, 450)
+        Me.ClientSize = New System.Drawing.Size(821, 559)
+        Me.Controls.Add(Me.btn_upload)
+        Me.Controls.Add(Me.btn_save_image)
+        Me.Controls.Add(Me.pic_box)
         Me.Controls.Add(Me.btn_open)
         Me.Controls.Add(Me.btn_save)
         Me.Controls.Add(Me.txt_box)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "FILE IO"
+        CType(Me.pic_box, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -71,4 +117,8 @@ Partial Class Form1
     Friend WithEvents txt_box As TextBox
     Friend WithEvents btn_save As Button
     Friend WithEvents btn_open As Button
+    Friend WithEvents ImageList1 As ImageList
+    Friend WithEvents pic_box As PictureBox
+    Friend WithEvents btn_upload As Button
+    Friend WithEvents btn_save_image As Button
 End Class
--- FILE_IO/Form1.vb
+++ FILE_IO/Form1.vb
@@ -1,16 +1,17 @@
 ﻿Imports System.IO
-Imports System.IO.File
-Imports System.Diagnostics
-Imports System.Security.Cryptography
-Imports System.Net.WebRequestMethods
 Imports System.Text
 
 Public Class Form1
     Dim open_Dlg As New OpenFileDialog
     Dim sv_Dlg As New SaveFileDialog
     Dim dir As String
+    Dim pic_name As String
 
 
+
+    Function DataStream()
+
+    End Function
 
 
     Private Sub btn_save_Click(sender As Object, e As EventArgs) Handles btn_save.Click
@@ -20,6 +21,7 @@ Public Class Form1
             If (IO.File.Exists(dir) = False) Then
                 sv_Dlg.Filter = "Text [*.txt*]|*.txt|All Files[*.*]|*.*"
                 sv_Dlg.Title = "save file location"
+                sv_Dlg.FilterIndex = 1
                 If sv_Dlg.ShowDialog = Windows.Forms.DialogResult.OK Then
                     dir = sv_Dlg.FileName
                 End If
@@ -28,11 +30,12 @@ Public Class Form1
 
             Dim File As FileStream = New FileStream(dir, FileMode.Create, FileAccess.Write)
             Dim buffer As [Byte]() = Encoding.ASCII.GetBytes(txt_box.Text)
-                Dim MS As MemoryStream = New MemoryStream(buffer)
-                MS.WriteTo(File)
-                File.Close()
-                MS.Close()
-
+            Dim MS As MemoryStream = New MemoryStream(buffer)
+            MS.WriteTo(File)
+            File.Close()
+            MS.Close()
+            sv_Dlg.Dispose()
+            sv_Dlg.Reset()
 
             MsgBox("File saved!")
             txt_box.Clear()
@@ -68,4 +71,43 @@ Public Class Form1
             End Try
         End If
     End Sub
+
+    Private Sub btn_upload_Click(sender As Object, e As EventArgs) Handles btn_upload.Click
+
+        With open_Dlg
+            .InitialDirectory = "C:\"
+            .Filter = "JPEGs|*.jpg|GIFs|*.gif|Bitmaps|*.bmp|AllFiles|*.*"
+            .FilterIndex = 1
+            .Title = "Select Image"
+        End With
+
+        If open_Dlg.ShowDialog = Windows.Forms.DialogResult.OK Then
+            pic_box.Image = Image.FromFile(open_Dlg.FileName)
+            pic_box.SizeMode = PictureBoxSizeMode.StretchImage
+        End If
+
+
+
+
+
+    End Sub
+
+    Private Sub btn_save_image_Click(sender As Object, e As EventArgs) Handles btn_save_image.Click
+        With sv_Dlg
+            .InitialDirectory = "C:\"
+            .Filter = "JPEG|*.jpg|PNG|*.png|GIFs|*.gif|Bitmaps|*.bmp|AllFiles|*.*"
+            .FilterIndex = 1
+            .Title = "Saving Image - Select Location"
+        End With
+
+        If sv_Dlg.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Using pic As New Bitmap(open_Dlg.FileName)
+                pic.Save(IO.Path.Combine(Path.GetDirectoryName(sv_Dlg.FileName), Path.GetFileName(sv_Dlg.FileName)))
+            End Using
+            pic_box.Image = Nothing
+        End If
+    End Sub
+
+
+
 End Class
