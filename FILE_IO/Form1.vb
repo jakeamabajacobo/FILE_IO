cded9bb8cb059dfdef6ee883c20555d64493eb2f
J Jacobs <jakejacobo2015@gmail.com>
2023-05-25 12:59:45 +0800

    update test new data

--- FILE_IO/Form1.vb
+++ FILE_IO/Form1.vb
@@ -7,8 +7,7 @@ Imports System.Text
 
 Public Class Form1
     Dim open_Dlg As New OpenFileDialog
-    Dim strm_rdr As StreamReader
-    Dim strm_wtr As StreamWriter
+    Dim sv_Dlg As New SaveFileDialog
     Dim dir As String
 
 
@@ -16,60 +15,29 @@ Public Class Form1
 
     Private Sub btn_save_Click(sender As Object, e As EventArgs) Handles btn_save.Click
         Try
-            If (IO.File.Exists(dir)) Then
 
-                Try
-                    Dim buffer As [Byte]() = Encoding.ASCII.GetBytes(txt_box.Text)
-                    Dim MS As MemoryStream = New MemoryStream(buffer)
-
-                    If (IO.File.Exists(dir)) Then
-                        Dim File As FileStream = New FileStream(dir, FileMode.Create, FileAccess.Write)
-                    Else
-
-                        If sv_dialog.ShowDialog = Windows.Forms.DialogResult.OK Then
-
-
-                        End If
-                    End If
-
-                
-
-                    MS.WriteTo(File)
-                    File.Close()
-                    MS.Close()
-
-
-                    MsgBox("File saved!")
-                    txt_box.Clear()
-
-                Catch ex As Exception
-                    MsgBox(ex.Message)
-                End Try
-
-
-            Else
-                Dim sv_dialog As New SaveFileDialog
-
-                sv_dialog.Filter = "TXT Files (*.txt*)|*.txt"
-
-                If sv_dialog.ShowDialog = Windows.Forms.DialogResult.OK Then
-                    My.Computer.FileSystem.WriteAllText(sv_dialog.FileName, txt_box.Text, False)
+            'NEW TEXT
+            If (IO.File.Exists(dir) = False) Then
+                sv_Dlg.Filter = "Text [*.txt*]|*.txt|All Files[*.*]|*.*"
+                sv_Dlg.Title = "save file location"
+                If sv_Dlg.ShowDialog = Windows.Forms.DialogResult.OK Then
+                    dir = sv_Dlg.FileName
                 End If
-
-
-
-                MsgBox("File saved!")
-                txt_box.Clear()
-
-
             End If
 
 
+            Dim File As FileStream = New FileStream(dir, FileMode.Create, FileAccess.Write)
+            Dim buffer As [Byte]() = Encoding.ASCII.GetBytes(txt_box.Text)
+                Dim MS As MemoryStream = New MemoryStream(buffer)
+                MS.WriteTo(File)
+                File.Close()
+                MS.Close()
 
 
+            MsgBox("File saved!")
+            txt_box.Clear()
 
 
-            ' strm_writer.Close()
         Catch ex As Exception
             MsgBox(ex.Message)
         End Try
